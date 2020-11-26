213602bfe048d886a4b2098c90c80aa6df066902
WendiF <wendi.fan@softwire.com>
2020-11-26 23:19:59 +0000

    Added view members tab with modified display

---
+++ src/main/java/org/softwire/training/bookish/models/database/Member.java
@@ -0,0 +1,21 @@
+package org.softwire.training.bookish.models.database;
+
+public class Member {
+    private int id;
+    private String name;
+    private String logoUrl;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
