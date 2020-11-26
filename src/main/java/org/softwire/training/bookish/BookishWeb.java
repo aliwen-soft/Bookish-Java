48ff89179dbdb109c552eecdb05a5a6935d3a554
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-26 16:39:45 +0000

    update to services

--- src/main/java/org/softwire/training/bookish/BookishWeb.java
+++ src/main/java/org/softwire/training/bookish/BookishWeb.java
@@ -1,7 +1,14 @@
 package org.softwire.training.bookish;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
+import org.softwire.training.bookish.models.database.Book;
+import org.softwire.training.bookish.models.database.Technology;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 /**
  * Entry point to the Bookish Application
@@ -11,4 +18,5 @@ public class BookishWeb {
     public static void main(String[] args) {
         SpringApplication.run(BookishWeb.class, args);
     }
+
 }
