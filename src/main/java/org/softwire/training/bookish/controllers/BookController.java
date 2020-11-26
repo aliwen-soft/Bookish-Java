53e55cbcbf30ffa32492dbeddd92485d9da387fd
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-26 16:53:53 +0000

    book controller

---
+++ src/main/java/org/softwire/training/bookish/controllers/BookController.java
@@ -0,0 +1,56 @@
+package org.softwire.training.bookish.controllers;
+
+import org.softwire.training.bookish.models.database.Book;
+import org.softwire.training.bookish.models.database.Technology;
+import org.softwire.training.bookish.models.page.AboutPageModel;
+import org.softwire.training.bookish.models.page.BookPageModel;
+import org.softwire.training.bookish.services.BookService;
+import org.softwire.training.bookish.services.TechnologyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("")
+    ModelAndView bookPage() {
+
+        List<Book> allBooks = bookService.getAllBooks();
+
+        BookPageModel bookPageModel = new BookPageModel();
+        bookPageModel.setBooks(allBooks);
+
+        return new ModelAndView("book", "model", bookPageModel);
+    }
+
+    @RequestMapping("/add-book")
+    RedirectView addBook(@ModelAttribute Book book) {
+
+        bookService.addBook(book);
+
+        return new RedirectView("/books");
+    }
+
+    @RequestMapping("/delete-book")
+    RedirectView deleteBook(@RequestParam String isbn ) {
+
+        bookService.deleteBook(isbn);
+
+        return new RedirectView("/books");
+    }
+}
