0ade5e48361ef0b30c2013b8d9ea759093fd9937
Joseph Silcock <joseph.silcock@softwire.com>
2020-11-27 12:22:18 +0000

    Bug fix

--- src/main/java/org/softwire/training/bookish/models/page/BookAuthorPageModel.java
+++ src/main/java/org/softwire/training/bookish/models/page/BookAuthorPageModel.java
@@ -1,6 +1,6 @@
 package org.softwire.training.bookish.models.page;
 
-public class BookAuthorPageModel extends BookPageModel {
+public class BookAuthorPageModel extends BooksPageModel {
     private String author;
 
     public String getAuthor() {
