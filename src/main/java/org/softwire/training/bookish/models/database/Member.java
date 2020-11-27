82d30b8991e62f1f7ddaea4ab1910a8dcb52f0b0
Joseph Silcock <joseph.silcock@softwire.com>
2020-11-27 14:27:39 +0000

    Tidied code

--- src/main/java/org/softwire/training/bookish/models/database/Member.java
+++ src/main/java/org/softwire/training/bookish/models/database/Member.java
@@ -1,21 +1,11 @@
 package org.softwire.training.bookish.models.database;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Member {
     private int id;
     private String name;
-    private String logoUrl;
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
