4897644cf727fb054f5e2f4ee63ee7c2aa7d6522
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-27 09:39:26 +0000

    Set up lomboc

--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,39 +1,16 @@
 package org.softwire.training.bookish.models.database;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.beans.ConstructorProperties;
 
-
+@Getter
+@Setter
 public class Book {
     private String isbn;
     private String name;
     private String description;
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     public void display() {
         System.out.println(isbn + ", " + name + ", " + description);
