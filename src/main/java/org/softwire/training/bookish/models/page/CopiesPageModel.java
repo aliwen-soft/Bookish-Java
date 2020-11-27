a3897ea0d09d415812381cf4b83091e48e6a98b4
Joseph Silcock <joseph.silcock@softwire.com>
2020-11-27 14:06:14 +0000

    Started to add book copies list

---
+++ src/main/java/org/softwire/training/bookish/models/page/CopiesPageModel.java
@@ -0,0 +1,13 @@
+package org.softwire.training.bookish.models.page;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.softwire.training.bookish.models.database.BookCopy;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CopiesPageModel {
+    private List<BookCopy> copies;
+}
