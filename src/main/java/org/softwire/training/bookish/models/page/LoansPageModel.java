8d1f6703de393c5030b27d6e1f6b9c67f2ff08e3
WendiF <wendi.fan@softwire.com>
2020-11-27 12:31:48 +0000

    Members Dropdown in loans

--- src/main/java/org/softwire/training/bookish/models/page/LoansPageModel.java
+++ src/main/java/org/softwire/training/bookish/models/page/LoansPageModel.java
@@ -1,16 +1,27 @@
 package org.softwire.training.bookish.models.page;
 
 import org.softwire.training.bookish.models.database.Loan;
+import org.softwire.training.bookish.models.database.Member;
 
 import java.util.List;
 
 public class LoansPageModel {
     private List<Loan> loans;
+    private List<Member> members;
 
     public List<Loan> getLoans() {
         return loans;
     }
+
     public void setLoans(List<Loan> loans) {
         this.loans = loans;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
