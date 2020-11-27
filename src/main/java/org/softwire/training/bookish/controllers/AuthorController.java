269814481af3b39f7206f522a1b245ec68feacd1
Joseph Silcock <joseph.silcock@softwire.com>
2020-11-27 12:07:05 +0000

    Tidied imports

--- src/main/java/org/softwire/training/bookish/controllers/AuthorController.java
+++ src/main/java/org/softwire/training/bookish/controllers/AuthorController.java
@@ -1,11 +1,8 @@
 package org.softwire.training.bookish.controllers;
 
 import org.softwire.training.bookish.models.database.Author;
-import org.softwire.training.bookish.models.database.Technology;
-import org.softwire.training.bookish.models.page.AboutPageModel;
 import org.softwire.training.bookish.models.page.AuthorPageModel;
 import org.softwire.training.bookish.services.AuthorService;
-import org.softwire.training.bookish.services.TechnologyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
